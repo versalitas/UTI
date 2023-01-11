18b50ec354b47f93f889f85ce66e63ce7c0fcefe
MK <caprichismo@gmail.com>
2023-01-11 19:19:17 +0100

    new beginnings

---
+++ sharedState.js
@@ -0,0 +1,24 @@
+let allMessages = [];
+let allClients = [];
+let users = [];
+let messageSceneCounter = 0;
+let sceneNumber = 0;
+let rolesAsigned = 0;
+let rolesAr = [];
+let lastMessageSend = 0;
+let timeBetweenMessages = 15000;
+let screenPosition = 1;
+let soundDirector = 0;
+let blockNewMessages = false;
+let csvSounds = [];
+let totalSampleAudios = 70;
+let colorCounter = 0;
+
+
+module.exports = { allMessages, allClients, users, 
+    messageSceneCounter, sceneNumber, rolesAssigned, rolesAr, 
+    lastMessageSend, timeBetweenMessages, screenPosition, 
+    soundDirector, blockNewMessages, csvSounds, 
+    totalSampleAudios, colorCounter}
+
+}
